5b2d69ddded583f4147a0464ba86154e208617f2
Quagmire23 <85202866+Quagmire23@users.noreply.github.com>
2021-06-16 19:01:44 -0700

    Add files via upload

---
+++ Junto-main/ProyectoSimulacionOriginal/ProyectoSimulacionOriginal/Tinas.Designer.cs
@@ -0,0 +1,369 @@
+﻿
+namespace ProyectoSimulacionOriginal
+{
+    partial class Tinas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtcostodetransporte = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txttinasporcorrida = new System.Windows.Forms.TextBox();
+            this.lbldecision = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txttotaldecorridas = new System.Windows.Forms.TextBox();
+            this.btnrentacamion = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtcostoderentadecamion = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txttotalderenta = new System.Windows.Forms.TextBox();
+            this.txtvecesqueserenta = new System.Windows.Forms.TextBox();
+            this.txttotaldiasderenta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtmedia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtpesotina2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtpesotina1 = new System.Windows.Forms.TextBox();
+            this.btngenerarT = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.c1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(719, 35);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(99, 13);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "Costo de transporte";
+            // 
+            // txtcostodetransporte
+            // 
+            this.txtcostodetransporte.Location = new System.Drawing.Point(718, 55);
+            this.txtcostodetransporte.Name = "txtcostodetransporte";
+            this.txtcostodetransporte.Size = new System.Drawing.Size(100, 20);
+            this.txtcostodetransporte.TabIndex = 48;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 192);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 13);
+            this.label9.TabIndex = 47;
+            this.label9.Text = "Número de tinas por corrida";
+            // 
+            // txttinasporcorrida
+            // 
+            this.txttinasporcorrida.Location = new System.Drawing.Point(190, 185);
+            this.txttinasporcorrida.Name = "txttinasporcorrida";
+            this.txttinasporcorrida.Size = new System.Drawing.Size(100, 20);
+            this.txttinasporcorrida.TabIndex = 46;
+            // 
+            // lbldecision
+            // 
+            this.lbldecision.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldecision.Location = new System.Drawing.Point(556, 185);
+            this.lbldecision.Name = "lbldecision";
+            this.lbldecision.Size = new System.Drawing.Size(221, 40);
+            this.lbldecision.TabIndex = 45;
+            this.lbldecision.Text = "Decisión";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Total de corridas";
+            // 
+            // txttotaldecorridas
+            // 
+            this.txttotaldecorridas.Location = new System.Drawing.Point(190, 146);
+            this.txttotaldecorridas.Name = "txttotaldecorridas";
+            this.txttotaldecorridas.Size = new System.Drawing.Size(100, 20);
+            this.txttotaldecorridas.TabIndex = 43;
+            // 
+            // btnrentacamion
+            // 
+            this.btnrentacamion.Location = new System.Drawing.Point(608, 153);
+            this.btnrentacamion.Name = "btnrentacamion";
+            this.btnrentacamion.Size = new System.Drawing.Size(75, 23);
+            this.btnrentacamion.TabIndex = 42;
+            this.btnrentacamion.Text = "Resultados";
+            this.btnrentacamion.UseVisualStyleBackColor = true;
+            this.btnrentacamion.Click += new System.EventHandler(this.btnrentacamion_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(433, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 13);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Costo de renta de camión";
+            // 
+            // txtcostoderentadecamion
+            // 
+            this.txtcostoderentadecamion.Location = new System.Drawing.Point(454, 55);
+            this.txtcostoderentadecamion.Name = "txtcostoderentadecamion";
+            this.txtcostoderentadecamion.Size = new System.Drawing.Size(100, 20);
+            this.txtcostoderentadecamion.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(733, 106);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "Total de renta";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(590, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Veces que se renta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(451, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 13);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "Total de dias de renta";
+            // 
+            // txttotalderenta
+            // 
+            this.txttotalderenta.Location = new System.Drawing.Point(718, 125);
+            this.txttotalderenta.Name = "txttotalderenta";
+            this.txttotalderenta.Size = new System.Drawing.Size(100, 20);
+            this.txttotalderenta.TabIndex = 36;
+            // 
+            // txtvecesqueserenta
+            // 
+            this.txtvecesqueserenta.Location = new System.Drawing.Point(589, 125);
+            this.txtvecesqueserenta.Name = "txtvecesqueserenta";
+            this.txtvecesqueserenta.Size = new System.Drawing.Size(100, 20);
+            this.txtvecesqueserenta.TabIndex = 35;
+            // 
+            // txttotaldiasderenta
+            // 
+            this.txttotaldiasderenta.Location = new System.Drawing.Point(454, 126);
+            this.txttotaldiasderenta.Name = "txttotaldiasderenta";
+            this.txttotaldiasderenta.Size = new System.Drawing.Size(100, 20);
+            this.txttotaldiasderenta.TabIndex = 34;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Media";
+            // 
+            // txtmedia
+            // 
+            this.txtmedia.Location = new System.Drawing.Point(190, 68);
+            this.txtmedia.Name = "txtmedia";
+            this.txtmedia.Size = new System.Drawing.Size(100, 20);
+            this.txtmedia.TabIndex = 32;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Peso maximo por tina";
+            // 
+            // txtpesotina2
+            // 
+            this.txtpesotina2.Location = new System.Drawing.Point(190, 103);
+            this.txtpesotina2.Name = "txtpesotina2";
+            this.txtpesotina2.Size = new System.Drawing.Size(100, 20);
+            this.txtpesotina2.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Peso minimo por tina";
+            // 
+            // txtpesotina1
+            // 
+            this.txtpesotina1.Location = new System.Drawing.Point(190, 32);
+            this.txtpesotina1.Name = "txtpesotina1";
+            this.txtpesotina1.Size = new System.Drawing.Size(100, 20);
+            this.txtpesotina1.TabIndex = 28;
+            // 
+            // btngenerarT
+            // 
+            this.btngenerarT.Location = new System.Drawing.Point(30, 216);
+            this.btngenerarT.Name = "btngenerarT";
+            this.btngenerarT.Size = new System.Drawing.Size(75, 23);
+            this.btngenerarT.TabIndex = 27;
+            this.btngenerarT.Text = "Generar";
+            this.btngenerarT.UseVisualStyleBackColor = true;
+            this.btngenerarT.Click += new System.EventHandler(this.btngenerarT_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c1,
+            this.c6,
+            this.c2,
+            this.c3,
+            this.c4,
+            this.c5});
+            this.dataGridView1.Location = new System.Drawing.Point(30, 245);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(653, 299);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // c1
+            // 
+            this.c1.HeaderText = "Corrida";
+            this.c1.Name = "c1";
+            // 
+            // c6
+            // 
+            this.c6.HeaderText = "Tina";
+            this.c6.Name = "c6";
+            // 
+            // c2
+            // 
+            this.c2.HeaderText = "Número pseoudoaleatorio";
+            this.c2.Name = "c2";
+            // 
+            // c3
+            // 
+            this.c3.HeaderText = "Peso de tina";
+            this.c3.Name = "c3";
+            // 
+            // c4
+            // 
+            this.c4.HeaderText = "Peso acumulado";
+            this.c4.Name = "c4";
+            // 
+            // c5
+            // 
+            this.c5.HeaderText = "Rentar o no";
+            this.c5.Name = "c5";
+            // 
+            // Tinas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 614);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtcostodetransporte);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txttinasporcorrida);
+            this.Controls.Add(this.lbldecision);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txttotaldecorridas);
+            this.Controls.Add(this.btnrentacamion);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtcostoderentadecamion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txttotalderenta);
+            this.Controls.Add(this.txtvecesqueserenta);
+            this.Controls.Add(this.txttotaldiasderenta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtmedia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtpesotina2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpesotina1);
+            this.Controls.Add(this.btngenerarT);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Tinas";
+            this.Text = "Tinas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtcostodetransporte;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txttinasporcorrida;
+        private System.Windows.Forms.Label lbldecision;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txttotaldecorridas;
+        private System.Windows.Forms.Button btnrentacamion;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtcostoderentadecamion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txttotalderenta;
+        private System.Windows.Forms.TextBox txtvecesqueserenta;
+        private System.Windows.Forms.TextBox txttotaldiasderenta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtmedia;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtpesotina2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtpesotina1;
+        private System.Windows.Forms.Button btngenerarT;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c5;
+    }
+}
---
+++ Junto-main/ProyectoSimulacionOriginal/ProyectoSimulacionOriginal/Tinas.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoSimulacionOriginal
+{
+    public partial class Tinas : Form
+    {
+        //variables Tinas
+        public static double a = 101;
+        public static double c = 221;
+        public static double Xo = 17;
+        public static double M = 17001;
+        public static int i;
+        public static int b;
+        public static double numerodetinas = 4;
+        public static double modulo, m = 0, acumulador = 0;
+        public static int ad;
+        public static double[] random = new double[100000];
+        public static double t1;
+        public static double media;
+        public static double t2;
+        public static double costo_de_transporte;
+        public static double peso_de_tina;
+        public static double redondeado;
+        public static double peso_acumulado_tinas;
+        public static double numcorridas;
+        public static double vecesqueserenta = 0;
+
+        private void btngenerarT_Click(object sender, EventArgs e)
+        {
+            double.TryParse(txttotaldecorridas.Text, out numcorridas);
+            double.TryParse(txttinasporcorrida.Text, out numerodetinas);
+
+            for (i = 1; i <= numcorridas; i++)
+            {
+                peso_acumulado_tinas = 0;
+                ad = dataGridView1.Rows.Add();
+                dataGridView1.Rows[ad].Cells[0].Value = (i).ToString();
+
+                for (b = 1; b <= numerodetinas; b++)
+                {
+
+                    ad = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[ad].Cells[1].Value = (b).ToString();
+                    modulo = (a * Xo + c) % M;
+                    random[b] = modulo / M;
+                    double redoneado = (Math.Round(random[b], 5));
+                    dataGridView1.Rows[ad].Cells[2].Value = redoneado.ToString();
+                    acumulador += random[b];
+                    PesoDeTina(redoneado);
+                    m = modulo;
+                    Xo = m;
+
+                }
+                txttotaldiasderenta.Text = numcorridas.ToString();
+                Rentar();
+
+            }
+        }
+
+        private void btnrentacamion_Click(object sender, EventArgs e)
+        {
+
+            double costoderentadecamion;
+            double totaldedediasderenta;
+            double vecesqueserenta;
+            double costotalderenta;
+
+
+            double.TryParse(txtcostoderentadecamion.Text, out costoderentadecamion);
+            double.TryParse(txttotaldiasderenta.Text, out totaldedediasderenta);
+            double.TryParse(txtvecesqueserenta.Text, out vecesqueserenta);
+            double.TryParse(txtcostodetransporte.Text, out costo_de_transporte);
+
+            if (totaldedediasderenta == vecesqueserenta)
+            {
+                costotalderenta = costo_de_transporte * totaldedediasderenta;
+                txttotalderenta.Text = costotalderenta.ToString();
+            }
+            else
+            {
+                costotalderenta = costo_de_transporte * vecesqueserenta;
+                txttotalderenta.Text = costotalderenta.ToString();
+            }
+
+            if (costotalderenta <= costoderentadecamion)
+            {
+                lbldecision.Text = "Sigue rentando camión";
+            }
+            else
+            {
+                lbldecision.Text = "Compra nuevo camión";
+            }
+        }
+
+        public void PesoAcumulado()
+        {
+            peso_acumulado_tinas = peso_acumulado_tinas + peso_de_tina;
+
+            dataGridView1.Rows[ad].Cells[4].Value = Math.Round(peso_acumulado_tinas, 5).ToString();
+
+        }
+
+        public void Rentar()
+        {
+            if (peso_acumulado_tinas < 1000)
+            {
+                dataGridView1.Rows[ad].Cells[5].Value = "Rentar";
+            }
+            else
+            {
+
+                dataGridView1.Rows[ad].Cells[5].Value = "No rentar";
+            }
+            if (dataGridView1.Rows[ad].Cells[5].Value.ToString() == "Rentar")
+            {
+                vecesqueserenta = vecesqueserenta + 1;
+                txtvecesqueserenta.Text = vecesqueserenta.ToString();
+            }
+
+        }
+        public void PesoDeTina(double redondeado)
+        {
+            double.TryParse(txtpesotina1.Text, out t1);
+            double.TryParse(txtmedia.Text, out media);
+            double.TryParse(txtpesotina2.Text, out t2);
+
+            double basetriangulomayor;
+            double baseprimertriangulomenor;
+            double basesegundotriangulomenor;
+            double proporciondeltrianguodebase1;
+            double proporciondeltrianguodebase2;
+
+            basetriangulomayor = (t2 - t1);
+            baseprimertriangulomenor = (media - t1);
+            basesegundotriangulomenor = (t2 - media);
+
+            proporciondeltrianguodebase1 = (baseprimertriangulomenor / basetriangulomayor);
+            proporciondeltrianguodebase2 = (basesegundotriangulomenor / basetriangulomayor);
+
+
+            if (redondeado < proporciondeltrianguodebase1)
+            {
+                peso_de_tina = t1 + Math.Sqrt((t2 - t1) * (media - t1) * redondeado);
+                PesoAcumulado();
+                dataGridView1.Rows[ad].Cells[3].Value = Math.Round(peso_de_tina, 5).ToString();
+            }
+            else if (redondeado >= proporciondeltrianguodebase1)
+            {
+                peso_de_tina = t2 - Math.Sqrt((t2 - t1) * (t2 - media) * (1 - redondeado));
+                PesoAcumulado();
+                dataGridView1.Rows[ad].Cells[3].Value = Math.Round(peso_de_tina, 5).ToString();
+            }
+            else if (redondeado == proporciondeltrianguodebase1)
+            {
+                peso_de_tina = t1 + Math.Sqrt((t2 - t1) * (media - t1) * redondeado);
+                peso_de_tina = t2 - Math.Sqrt((t2 - t1) * (t2 - media) * (1 - redondeado));
+            }
+        }
+
+        public Tinas()
+        {
+            InitializeComponent();
+        }
+    }
+}
